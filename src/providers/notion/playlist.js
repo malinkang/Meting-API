c2c5a691350a1188b56f0ff1aa2d3803a44ee93a
malinkang <malinkang@adsconflux.com>
2025-06-10 18:05:45 +0800

    优化代码

--- src/providers/notion/playlist.js
+++ src/providers/notion/playlist.js
@@ -4,7 +4,8 @@ import { map_song_list } from "./util.js"
 import { Client } from "@notionhq/client"
 
 export const get_playlist = async (id) => {
-    const notion = new Client({ auth: process.env.NOTION_TOKEN })
+    // const notion = new Client({ auth: process.env.NOTION_TOKEN })
+    const notion = new Client({ auth: "secret_xvMkQzLkCRtZL478L8MhvLdIDOxicjjSUm9U9voAwbb"})
     let res = []
     let has_more = true
     let start_cursor = undefined
@@ -13,31 +14,15 @@ export const get_playlist = async (id) => {
         start_cursor: start_cursor,
         page_size: 100,
     })
-
+    console.log(`queryRes = ${queryRes}`)
     for (const page of queryRes.results) {
         const properties = page.properties
-        // 处理歌手relation
-        const relation = properties?.歌手?.relation || [];
-        let author = '';
-        if (relation.length > 0) {
-            const authorNames = [];
-            for (const rel of relation) {
-                try {
-                    const artistPage = await notion.pages.retrieve({ page_id: rel.id });
-                    const artistName = artistPage.properties?.标题?.title?.[0]?.plain_text || '';
-                    if (artistName) authorNames.push(artistName);
-                } catch (e) {
-                    console.error(`获取歌手页面失败: ${rel.id}`, e);
-                }
-            }
-            author = authorNames.join('&');
-        }
         res.push({
             title: properties?.歌曲?.title?.[0]?.plain_text || '',
             url: properties?.音频?.files?.[0]?.file?.url || '',
             lrc: properties?.歌词?.files?.[0]?.file?.url || '',
             pic: page.cover?.external?.url || page.cover?.file?.url || '',
-            author: author
+            author: properties?.Artist?.formula?.string || ''
         })
     }
 
--- src/providers/notion/song.js
+++ src/providers/notion/song.js
@@ -43,29 +43,12 @@ export const get_song_info = async (id, cookie = '') => {
         // 这里假设Notion页面属性中有音乐相关信息
         // 你可以根据实际Notion数据库结构调整字段
         const properties = page.properties
-        const relation = properties?.歌手?.relation || [];
-        let author = '';
-        if (relation.length > 0) {
-            // 遍历relation，根据id获取关联的page
-            const authorNames = [];
-            for (const rel of relation) {
-                try {
-                    const artistPage = await notion.pages.retrieve({ page_id: rel.id });
-                    // 假设歌手页面有“姓名”属性
-                    const artistName = artistPage.properties?.标题?.title?.[0]?.plain_text || '';
-                    if (artistName) authorNames.push(artistName);
-                } catch (e) {
-                    console.error(`获取歌手页面失败: ${rel.id}`, e);
-                }
-            }
-            author = authorNames.join('&');
-        }
         res = [{
             title: properties?.歌曲?.title?.[0]?.plain_text || '',
             url: properties?.音频?.files?.[0]?.file?.url || '', // 音频为文件类型
             lrc: page.properties?.歌词?.files?.[0]?.file?.url || '', // lrc属性名为格式，也是文件类型
             pic: page.cover?.external?.url || page.cover?.file?.url || '', // 获取页面封面作为pic
-            author:author
+            author: properties?.Artist?.formula?.string || ''
         }]
     } catch (e) {
         console.error(e)
